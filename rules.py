6eb7422b027c003897b6c55e0a57edbcfd7f66c1
Gautam Veldanda <veldandaga@outlook.com>
2022-11-23 03:27:03 -0800

    Create rules.py

---
+++ rules.py
@@ -0,0 +1,191 @@
+# Preferences of the user
+
+preferences = {
+    "appearance": {
+        {
+            "name": "gender",
+            "criteria": "many",
+            "values": ["male", "female", "other"],
+            "score": 5,
+        },
+        {
+            "name": "height",
+            "criteria": "range",
+            "values": [150, 200],
+            "score": 4,
+        },
+        {
+            "name": "age",
+            "criteria": "range",
+            "values": [18, 30],
+            "score": 5,
+        },
+        {
+            "name": "hair",
+            "criteria": "many",
+            "values": ["brown", "blonde", "black", "red", "grey"],
+            "score": 3,
+        },
+        {
+            "name": "eyes",
+            "criteria": "many",
+            "values": ["brown", "blue", "green", "grey"],
+            "score": 3,
+        },
+    },
+    "lifestyle": {
+        {
+            "name": "hobbies",
+            "criteria": "many",
+            "values": ["sports", "reading", "music", "movies", "games"],
+            "score": 5,
+        },
+        {
+            "name": "smoking",
+            "criteria": "one",
+            "values": ["no"],
+            "score": 5,
+        },
+        {
+            "name": "drinking",
+            "criteria": "one",
+            "values": ["yes"],
+            "score": 4,
+        },
+        {
+            "name": "religion",
+            "criteria": "many",
+            "values": [
+                "christian",
+                "muslim",
+                "jewish",
+                "buddhist",
+                "hindu",
+                "atheist",
+                "other",
+            ],
+            "score": 5,
+        },
+        {
+            "name": "politics",
+            "criteria": "one",
+            "values": ["left", "right", "center", "other"],
+            "score": 5,
+        },
+        {
+            "name": "relationship",
+            "criteria": "one",
+            "values": ["casual"],
+            "score": 5,
+        },
+        {
+            "name": "children",
+            "criteria": "one",
+            "values": ["maybe"],
+            "score": 5,
+        },
+        {
+            "name": "pets",
+            "criteria": "one",
+            "values": ["no"],
+            "score": 5,
+        },
+    },
+    "background": {
+        {
+            "name": "education",
+            "criteria": "many",
+            "values": ["college", "university", "masters"],
+            "score": 5,
+        },
+        {
+            "name": "occupation",
+            "criteria": "many",
+            "values": ["student", "employed"],
+            "score": 5,
+        },
+    },
+    "interests": {
+        {
+            "name": "tags",
+            "criteria": "many",
+            "values": ["stranger things", "kanye west", "star wars", "cats"],
+            "score": 5,
+        },
+        {
+            "name": "music",
+            "criteria": "many",
+            "values": ["rock", "pop", "jazz", "classical", "metal", "other"],
+            "score": 5,
+        },
+        {
+            "name": "movies",
+            "criteria": "many",
+            "values": ["action", "comedy", "drama", "horror", "other"],
+            "score": 5,
+        },
+        {
+            "name": "tv",
+            "criteria": "many",
+            "values": ["action", "comedy", "drama", "horror", "other"],
+            "score": 5,
+        },
+        {
+            "name": "books",
+            "criteria": "many",
+            "values": ["fiction", "non-fiction", "other"],
+            "score": 5,
+        },
+        {
+            "name": "games",
+            "criteria": "many",
+            "values": ["action", "adventure", "rpg", "strategy", "other"],
+            "score": 5,
+        },
+        {
+            "name": "sports",
+            "criteria": "many",
+            "values": ["football", "basketball", "tennis", "other"],
+            "score": 5,
+        },
+        {
+            "name": "travel",
+            "criteria": "many",
+            "values": ["domestic", "international", "other"],
+            "score": 5,
+        },
+        {
+            "name": "food",
+            "criteria": "many",
+            "values": ["italian", "chinese", "indian", "other"],
+            "score": 5,
+        },
+        {
+            "name": "animals",
+            "criteria": "many",
+            "values": ["dogs", "cats", "birds", "other"],
+            "score": 5,
+        },
+        {
+            "name": "nature",
+            "criteria": "many",
+            "values": ["mountains", "beaches", "forests", "other"],
+            "score": 5,
+        },
+    },
+}
+
+# Characteristics of the user
+
+characteristics = {
+    "appearance": {
+        "gender": "male",
+        "height": 175,
+        "age": 25,
+        "hair": "black",
+        "eyes": "brown",
+    },
+    "lifestyle": {},
+    "background": {},
+    "interests": {},
+}
